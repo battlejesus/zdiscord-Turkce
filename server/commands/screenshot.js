74be1f7be1584397f9c63f540e4833da42c7716b
battlejesus <137706464+battlejesus@users.noreply.github.com>
2026-05-02 17:28:42 +0300

    Update screenshot.js

--- server/commands/screenshot.js
+++ server/commands/screenshot.js
@@ -14,38 +14,38 @@ const Buffer = require("buffer").Buffer;
 
 module.exports = {
     name: "screenshot",
-    description: "Oyuncunun ekranının görüntüsünü al",
+    description: "Screenshot player's POV",
     role: "god",
 
     options: [
         {
             name: "id",
-            description: "Oyuncunun mevcut ID'si",
+            description: "Player's current id",
             required: true,
             type: "INTEGER",
         },
     ],
 
     run: async (client, interaction, args) => {
-        if (!GetPlayerName(args.id)) return interaction.reply({ content: "Bu ID geçersiz görünüyor.", ephemeral: true });
-        if (GetResourceState("screenshot-basic") !== "started") return interaction.reply({ content: "Bu komut çalışmak için citizenfx'in `screenshot-basic` kaynağına ihtiyaç duyuyor.", ephemeral: false });
-        await interaction.reply("Ekran görüntüsü alınıyor..");
+        if (!GetPlayerName(args.id)) return interaction.reply({ content: "This ID seems invalid.", ephemeral: true });
+        if (GetResourceState("screenshot-basic") !== "started") return interaction.reply({ content: "This command requires citizenfx's `screenshot-basic` to work", ephemeral: false });
+        await interaction.reply("Taking screenshot..");
         const name = `${client.utils.log.timestamp(true)}_${args.id}.jpg`;
         const data = await takeScreenshot(args.id).catch(error => {
             client.utils.log.error(error);
-            return interaction.editReply("**Ekran görüntüsü alınırken hata oluştu**");
+            return interaction.editReply("**Error requesting screenshot**");
         });
-        const buffer = new Buffer.from(data, "base64");
+        const buffer = Buffer.from(data, "base64");
         const embed = new client.Embed()
-            .setTitle(`${GetPlayerName(args.id)} Ekranı`)
+            .setTitle(`${GetPlayerName(args.id)}'s Screen`)
             .setImage(`attachment://${name}`)
-            .setFooter({ text: `Alınma Zamanı: ${client.utils.log.timestamp()}` });
+            .setFooter({ text: `Taken At ${client.utils.log.timestamp()}` });
         await interaction.editReply({ content: null, embeds: [ embed ], files: [ { attachment: buffer, name: name } ] }).catch(console.error);
         if (client.config.SaveScreenshotsToServer) {
             await fs.mkdir(`${client.root}/screenshots`, { recursive: true }).catch();
             await fs.writeFile(`${client.root}/screenshots/${name}`, data, { encoding: "base64", flag:"w+" }).catch(client.utils.log.error);
         }
-        return client.utils.log.info(`[${interaction.member.displayName}] ${GetPlayerName(args.id)} (${args.id}) adlı oyuncunun ekranının görüntüsünü aldı`);
+        return client.utils.log.info(`[${interaction.member.displayName}] Took a screenshot of ${GetPlayerName(args.id)}'s (${args.id}) screen`);
     },
 };
 
